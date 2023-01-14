53bdf4f75b2b3a4fd8dfb618a889101b7ab4add9
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-14 15:14:46 +0100

    hamburger Menu toggle

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let menuPassive = document.getElementById("hamburgerMenuIconPassive");
+let menuActive = document.getElementById("hamburgerMenuIconActive");
+let hamburgerNav = document.getElementById("hamburgerNav");
+
+menuPassive.addEventListener('click', () =>{
+    menuPassive.style.display="none";
+    menuActive.style.display="block";
+    hamburgerNav.style.display="block";
+});
+
+menuActive.addEventListener('click', () =>{
+    menuActive.style.display="none";
+    menuPassive.style.display="block";
+    hamburgerNav.style.display="none";
+})
