17e884dc6ea18eabe47f2c0c9be4dc498ad682d3
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-16 00:46:57 +0100

    contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,19 @@ menuActive.addEventListener('click', () =>{
     menuPassive.style.display="block";
     hamburgerNav.style.display="none";
 });
+
+
+/* ================= contact input ============== */
+
+let emailInput = document.getElementById("emailInput");
+let nameInput = document.getElementById("nameInput");
+let messageInput = document.getElementById("messageInput");
+let newsletter = document.getElementById("newsletterBox");
+let button = document.getElementById("button");
+
+button.addEventListener('click', () =>{
+    console.log(emailInput.value);
+    console.log(nameInput.value );
+    console.log(messageInput.value);
+    console.log(newsletterBox.checked);
+});
